b0ffec6d84eab3992a2a98724fd915301232db38
oscar <oscar@oscar-TOSH>
2014-03-04 19:59:29 +0100

    commit del proyectogithub para subir a github

---
+++ proyectogithub/src/proyectogithub/Proyectogithub.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package proyectogithub;
+
+/**
+ *
+ * @author oscar
+ */
+public class Proyectogithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    
+//    proyecto conectado con github intentamos subir esta primera prueba
+    
+//    segundo intento subir con push
+    
+    
+}
